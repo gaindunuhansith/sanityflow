ebed0f835571ea22fef25f319566526d6ef50929
ImethLochanaWeerasooriya <IT23577138@my.sliit.lk>
2026-03-07 21:30:57 +0530

    fix: fixed a logical issue in inventoryTransaction.ts file

--- backend/src/services/inventoryTransaction.service.ts
+++ backend/src/services/inventoryTransaction.service.ts
@@ -1,10 +1,37 @@
 import { InventoryTransaction } from "../models/inventoryTransaction.model.js";
 import type { IInventoryTransaction } from "../types/inventoryTransaction.type.js";
+import { Resource } from "../models/resource.model.js";
 
 export const createTransaction = async (
   data: Partial<IInventoryTransaction>
 ) => {
-  return await InventoryTransaction.create(data);
+
+  const resource = await Resource.findById(data.product);
+
+  if (!resource) {
+    throw new Error("Resource not found");
+  }
+
+  const quantity = data.quantity ?? 0;
+
+  if (data.type === "REMOVE") {
+
+    if (resource.quantity < quantity) {
+      throw new Error("Not enough stock available");
+    }
+
+    resource.quantity -= quantity;
+  }
+
+  if (data.type === "ADD") {
+    resource.quantity += quantity;
+  }
+
+  await resource.save();
+
+  const transaction = await InventoryTransaction.create(data);
+
+  return transaction;
 };
 
 export const getAllTransactions = async () => {

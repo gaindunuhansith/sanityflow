c847767d4f6789bbcf2381499a1c9bf4c7e2936f
LehanNawaratne <162142452+LehanNawaratne@users.noreply.github.com>
2026-04-05 22:02:57 +0530

    chore: add beneficiary slice and RTK Query API into store configuration

--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -7,6 +7,8 @@ import { distributionReducer } from '@/features/distribution/distributionSlice'
 import { distributionApi } from '@/features/distribution/distributionApi'
 import { driverReducer } from '@/features/driver/driverSlice'
 import { driverApi } from '@/features/driver/driverApi'
+import { beneficiaryReducer } from '@/features/beneficiary/beneficiarySlice'
+import { beneficiaryApi } from '@/features/beneficiary/beneficiaryApi'
 
 export const store = configureStore({
   reducer: {
@@ -14,13 +16,21 @@ export const store = configureStore({
     auth: authReducer,
     distribution: distributionReducer,
     driver: driverReducer,
+    beneficiary: beneficiaryReducer,
     [authApi.reducerPath]: authApi.reducer,
     [forumApi.reducerPath]: forumApi.reducer,
     [distributionApi.reducerPath]: distributionApi.reducer,
     [driverApi.reducerPath]: driverApi.reducer,
+    [beneficiaryApi.reducerPath]: beneficiaryApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(forumApi.middleware, authApi.middleware, distributionApi.middleware, driverApi.middleware),
+    getDefaultMiddleware().concat(
+      forumApi.middleware,
+      authApi.middleware,
+      distributionApi.middleware,
+      driverApi.middleware,
+      beneficiaryApi.middleware,
+    ),
 })
 
 export type RootState = ReturnType<typeof store.getState>
